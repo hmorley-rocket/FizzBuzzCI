a56082ca80193f11c24ccccd1c40c9ac29e36062
hmorley-rocket <hmorley@rocketsoftware.com>
2026-02-13 12:51:34 +0000

    Update cobmfurun command in test.sh

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@ set +e
 rm -f *.so *.int *.gnt *.o *debug.log *cobconfig*cfg *prep *.log *sys*.txt *.idy *lst *report.txt
 
 cob -vgzU -e "" fizzbuzz.cbl -C case -C 'verbose preprocess"mfupp" verbose endp'
-cobmfurun -dc:ansi -verbose -redirect:true fizzbuzz.so
+cobmfurun -verbose -redirect:true fizzbuzz.so
